c3a69bf05c600e5966c3446683ad70c1803543c9
Sofian Adelin <xndadelin@xndadelin.home>
2025-03-24 20:29:19 +0200

    change icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Hackovina",
   description: "Hackovina is an entry-level hackathon for high school students.",
-  icons: ["https://assets.hackclub.com/icon-rounded.svg"]
+  icons: ["Untitled.png"],
 };
 
 export default function RootLayout({
